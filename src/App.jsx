e0b78789afb964c5a99b87bb0a90258ffd1f0407
morad <KARKARMORAD29@GMAIL.COM>
2026-05-22 10:40:02 +0200

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+//import './App.css'
 
 function App() {
 
@@ -15,7 +15,15 @@ function App() {
 
   return (
     <>
+      <h1>Lista Prodotti</h1>
+      <ul>
+        {products.map((p, i) => (
+          <li key={i}>
+            <p>{p.name} ({p.price.toFixed(2)}€)</p>
 
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
