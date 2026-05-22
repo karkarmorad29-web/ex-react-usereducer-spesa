24dde6d0aaf0ef932bbe0be076015a40fc79d6e9
morad <KARKARMORAD29@GMAIL.COM>
2026-05-22 10:10:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
